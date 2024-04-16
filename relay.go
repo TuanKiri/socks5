99951d53e9ccd6cdb907a3060c6bdcc7aac6156e
Ezhov Artem <ejow-artem@yandex.ru>
2024-04-16 11:31:58 +0700

    data transferred metrics

--- relay.go
+++ relay.go
@@ -6,13 +6,13 @@ type closeWriter interface {
 	CloseWrite() error
 }
 
-func relay(dst io.Writer, src io.Reader) error {
-	_, err := io.Copy(dst, src)
+func relay(dst io.Writer, src io.Reader) (int64, error) {
+	n, err := io.Copy(dst, src)
 
 	if writer, ok := dst.(closeWriter); ok {
 		// send EOF for next io.Copy
 		writer.CloseWrite()
 	}
 
-	return err
+	return n, err
 }
